13eb8e34eef9e83587b3104adac07027e3ff1056
Mike Jackson <mike.jackson@bluequartz.net>
2010-08-12 17:07:23 -0400

    Updating license information

--- Code/TO79/plugins/EMMPM/Random.h
+++ Code/TO79/plugins/EMMPM/Random.h
@@ -9,6 +9,36 @@
  *
  *  Initial port from original research codes performed by John van der Zwaag
  *  (vanderz@gri.msstate.edu) under US Air Force contract XXX_XXXX-XXXX
+ *  The Original EM/MPM algorithm was developed by Mary L. Comer and is distributed
+under the BSD License.
+Copyright (c) <2010>, <Mary L. Comer>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of <Mary L. Comer> nor the names of its contributors may be
+used to endorse or promote products derived from this software without specific
+prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  *
  * *************************************************************************** */
 
@@ -30,8 +60,8 @@ public:
   }
 
   void setSeed(long s)
-  { 
-    seed = s; 
+  {
+    seed = s;
     next(); next(); next();
   }
   long getSeed()
@@ -40,7 +70,7 @@ public:
   double next()
   {
     long hi, lo;
-    
+
     // Based on code in "Random Number Generators: Good Ones are Hard to Find,"
     // by Stephen K. Park and Keith W. Miller in Communications of the ACM,
     // 31, 10 (Oct. 1988) pp. 1192-1201.
@@ -51,7 +81,7 @@ public:
       seed += VTK_K_M;
     return static_cast<double>(seed)/VTK_K_M;
   }
-  
+
   double next(double min, double max)
   {
     return (min + next()*(max-min));
