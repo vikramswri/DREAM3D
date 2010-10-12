be6b5ecc77e174180dc71b49fc0b2063909e6b9b
Mike Jackson <mike.jackson@bluequartz.net>
2010-10-12 17:18:37 -0400

    Cleaning up

--- Code/AIM/Common/SourceList.cmake
+++
@@ -1,8 +0,0 @@
-
-set (AIM_Common_SRCS
-    ${PROJECT_CODE_DIR}/AIM/Common/AIMImage.cpp
-)
-set (AIM_Common_HDRS
-    ${PROJECT_CODE_DIR}/AIM/Common/AIMArray.hpp
-    ${PROJECT_CODE_DIR}/AIM/Common/AIMImage.h   )
-cmp_IDE_SOURCE_PROPERTIES("AIM/Common" "${AIM_Common_HDRS}" "${AIM_Common_SRCS}" "0")
