609793b495b9b32e77f5001b6c25fff5f2a001b6
Mike Jackson <mike.jackson@bluequartz.net>
2011-01-10 22:48:42 -0500

    Fixing installation cmake rules for plugins

--- OSX_Tools/OSX_BundleTools.cmake
+++ OSX_Tools/OSX_BundleTools.cmake
@@ -3,14 +3,13 @@ macro(MakeOSXBundleApp target binary_dir osx_tools_dir)
     set(target ${target})
     set(binary_dir ${binary_dir})
     set(osx_tools_dir ${osx_tools_dir})
-
-  SET (OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT "${binary_dir}/OSX_Scripts/${target}_OSX_MakeStandAloneBundle.cmake")
- # SET (OSX_MAKE_STANDALONE_BUNDLE_BASH_SCRIPT "${binary_dir}/OS_X_Scripts/${target}_OSX_MakeStandAloneBundle.sh")
-
-  CONFIGURE_FILE("${osx_tools_dir}/CompleteBundle.cmake.in"
-    "${OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT}" @ONLY IMMEDIATE)
- 
-  install(SCRIPT "${OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT}")
+    
+    set (OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT "${binary_dir}/OSX_Scripts/${target}_OSX_MakeStandAloneBundle.cmake")
+    
+    CONFIGURE_FILE("${osx_tools_dir}/CompleteBundle.cmake.in"
+            "${OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT}" @ONLY IMMEDIATE)
+     
+    install(SCRIPT "${OSX_MAKE_STANDALONE_BUNDLE_CMAKE_SCRIPT}")
                     
 endmacro(MakeOSXBundleApp)
 
@@ -20,8 +19,8 @@ macro(MakeOSXTool target binary_dir osx_tools_dir)
     set(target ${target})
     set(binary_dir ${binary_dir})
     set(osx_tools_dir ${osx_tools_dir})
-
-    SET (OSX_MAKE_STANDALONE_TOOL_CMAKE_SCRIPT 
+    
+    set (OSX_MAKE_STANDALONE_TOOL_CMAKE_SCRIPT 
         "${binary_dir}/OS_X_Scripts/${target}_OSX_MakeStandAloneTool.cmake")
     
     CONFIGURE_FILE("${osx_tools_dir}/CompleteTool.cmake.in"
